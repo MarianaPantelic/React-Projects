ad2b5f2b3c66463aeac7843c576bd72c7eb58370
Mariana Pantelic <mari27line@yahoo.com>
2021-03-22 11:33:46 +0100

    Rendering products dynamic from array

--- e-shopper/src/index.js
+++ e-shopper/src/index.js
@@ -11,11 +11,57 @@ import Page404 from "./pages/404";
 
 
 const App = () => {
+
+  //if we want to fetch the data:
+
+  // useEffect(() => {
+      // here we can fetch the data
+  //}, [])
+
+  const products = [
+    {
+      id: 123451,
+      title: "title1",
+      price: 50,
+      imgUrl: "/images/home/product1.jpg"
+    },
+    {
+      id: 123452,
+      title: "title2",
+      price: 50,
+      imgUrl: "/images/home/product2.jpg"
+    },
+    {
+      id: 123453,
+      title: "title3",
+      price: 50,
+      imgUrl: "/images/home/product3.jpg"
+    },
+    {
+      id: 123454,
+      title: "title4",
+      price: 50,
+      imgUrl: "/images/home/product4.jpg"
+    },
+    {
+      id: 123455,
+      title: "title5",
+      price: 50,
+      imgUrl: "/images/home/product5.jpg"
+    },
+    {
+      id: 123456,
+      title: "title6",
+      price: 50,
+      imgUrl: "/images/home/product6.jpg"
+    }
+  ]
+
   return (
     <Router>
       <Switch>
         <Route exact path="/">
-          <Home />
+          <Home products = {products}/>
         </Route>
         <Route path = "/shop"><Shop/></Route>
         <Route path = "/blog"><Blog/></Route>
